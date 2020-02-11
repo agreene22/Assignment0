ca68d7518769369abd27703257780fe2dcb51223
Anna Greene <agreene@chapman.edu>
2020-02-11 20:41:33 +0000

    Hello World file

---
+++ HelloWorld.cpp
@@ -0,0 +1,14 @@
+/* Anna Greene
+ * 002314663
+ * agreene@chapman.edu
+ * 350-02
+ * Assignment 0 - Hello World
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv){
+  cout << "Hello World!" << endl;
+  return 0;
+}
